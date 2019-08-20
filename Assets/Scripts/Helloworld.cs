8b23371a446042b4d8fae1e2a4dfaad2a627db01
Your Name <hasss95@hotmail.com>
2019-08-20 11:16:50 +0200

    first

---
+++ Assets/Scripts/Helloworld.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+   
+    public GameObject redCube;
+
+    void Start()
+    {
+        
+        print("Hello World");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            print("Respect");
+        }
+
+   
+    }
+
+    public void YoWorld()
+    {
+        print("Yo World");
+    }
+
+    public void SpawnRedCube()
+
+    {
+        for (int i = 0; i < 300; i++)
+        {
+            Instantiate(redCube);
+        }
+       
+   }
+
+
+}
---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    private Rigidbody rb;
+    void Start()
+    {
+        print("hej");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * 50;
+        float v = Input.GetAxis("Vertical") * 50;
+
+        Vector3 vel = rb.velocity;
+        vel.x = h;
+        vel.z = v;
+        rb.velocity = vel;
+    }
+}
